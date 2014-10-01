169d8238a4e87c5d2d3d4f8109a8d97aeefd937f
Tobias Schulte <tobias.schulte@gliderpilot.de>
2014-10-01 21:52:13 +0200

    P2Resolver

---
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/repositories/resolver/P2Resolver.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.internal.artifacts.repositories.resolver;
+
+import com.google.common.collect.ImmutableSet;
+import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.ModuleComponentRepositoryAccess;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.parser.DescriptorParseContext;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.parser.GradlePomModuleDescriptorParser;
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.parser.MetaDataParser;
+import org.gradle.api.internal.artifacts.repositories.transport.RepositoryTransport;
+import org.gradle.api.internal.component.ArtifactType;
+import org.gradle.internal.Transformers;
+import org.gradle.internal.component.external.model.*;
+import org.gradle.internal.component.model.*;
+import org.gradle.internal.resolve.result.BuildableArtifactSetResolveResult;
+import org.gradle.internal.resolve.result.BuildableModuleComponentMetaDataResolveResult;
+import org.gradle.internal.resolve.result.DefaultResourceAwareResolveResult;
+import org.gradle.internal.resolve.result.ResourceAwareResolveResult;
+import org.gradle.internal.resource.ExternalResourceName;
+import org.gradle.internal.resource.LocallyAvailableExternalResource;
+import org.gradle.internal.resource.ResourceNotFoundException;
+import org.gradle.internal.resource.local.FileStore;
+import org.gradle.internal.resource.local.LocallyAvailableResourceFinder;
+
+import java.net.URI;
+import java.util.*;
+
+public class P2Resolver extends ExternalResourceResolver {
+    private final URI root;
+
+    public P2Resolver(String name, URI rootUri, RepositoryTransport transport,
+                         LocallyAvailableResourceFinder<ModuleComponentArtifactMetaData> locallyAvailableResourceFinder,
+                         FileStore<ModuleComponentArtifactMetaData> artifactFileStore) {
+        super(name, transport.isLocal(),
+                transport.getRepository(),
+                transport.getResourceAccessor(),
+                new ChainedVersionLister(new MavenVersionLister(transport.getRepository()), new ResourceVersionLister(transport.getRepository())),
+                locallyAvailableResourceFinder,
+                artifactFileStore);
+        this.root = rootUri;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("P2 repository '%s'", getName());
+    }
+
+    public URI getRoot() {
+        return root;
+    }
+
+    protected void doResolveComponentMetaData(DependencyMetaData dependency, ModuleComponentIdentifier moduleComponentIdentifier, BuildableModuleComponentMetaDataResolveResult result) {
+        resolveStaticDependency(dependency, moduleComponentIdentifier, result, super.createArtifactResolver());
+    }
+
+    protected boolean isMetaDataArtifact(ArtifactType artifactType) {
+        return artifactType == ArtifactType.MAVEN_POM;
+    }
+
+    @Override
+    protected MutableModuleComponentResolveMetaData processMetaData(MutableModuleComponentResolveMetaData metaData) {
+        if (metaData.getId().getVersion().endsWith("-SNAPSHOT")) {
+            metaData.setChanging(true);
+        }
+        return metaData;
+    }
+
+    private void resolveUniqueSnapshotDependency(DependencyMetaData dependency, ModuleComponentIdentifier module, BuildableModuleComponentMetaDataResolveResult result, MavenUniqueSnapshotModuleSource snapshotSource) {
+        resolveStaticDependency(dependency, module, result, createArtifactResolver(snapshotSource));
+        if (result.getState() == BuildableModuleComponentMetaDataResolveResult.State.Resolved) {
+            result.getMetaData().setSource(snapshotSource);
+        }
+    }
+
+    private boolean isSnapshotVersion(ModuleComponentIdentifier module) {
+        return module.getVersion().endsWith("-SNAPSHOT");
+    }
+
+    @Override
+    protected ExternalResourceArtifactResolver createArtifactResolver(ModuleSource moduleSource) {
+
+        if (moduleSource instanceof MavenUniqueSnapshotModuleSource) {
+            final String timestamp = ((MavenUniqueSnapshotModuleSource) moduleSource).getTimestamp();
+            return new MavenUniqueSnapshotExternalResourceArtifactResolver(super.createArtifactResolver(moduleSource), timestamp);
+        }
+
+        return super.createArtifactResolver(moduleSource);
+    }
+
+    @Override
+    protected IvyArtifactName getMetaDataArtifactName(String moduleName) {
+        return new DefaultIvyArtifactName(moduleName, "pom", "pom");
+    }
+
+    @Override
+    public boolean isM2compatible() {
+        return false;
+    }
+
+    public ModuleComponentRepositoryAccess getLocalAccess() {
+        return new P2LocalRepositoryAccess();
+    }
+
+    public ModuleComponentRepositoryAccess getRemoteAccess() {
+        return new P2RemoteRepositoryAccess();
+    }
+
+    @Override
+    protected MutableModuleComponentResolveMetaData createMetaDataForDependency(DependencyMetaData dependency) {
+        return new DefaultMavenModuleResolveMetaData(dependency);
+    }
+
+    protected MutableModuleComponentResolveMetaData parseMetaDataFromResource(LocallyAvailableExternalResource cachedResource, DescriptorParseContext context) {
+        return null;
+    }
+
+    protected static MavenModuleResolveMetaData mavenMetaData(ModuleComponentResolveMetaData metaData) {
+        return Transformers.cast(MavenModuleResolveMetaData.class).transform(metaData);
+    }
+
+    private class P2LocalRepositoryAccess extends LocalRepositoryAccess {
+        @Override
+        protected void resolveConfigurationArtifacts(ModuleComponentResolveMetaData module, ConfigurationMetaData configuration, BuildableArtifactSetResolveResult result) {
+            if (mavenMetaData(module).isKnownJarPackaging()) {
+                ModuleComponentArtifactMetaData artifact = module.artifact("jar", "jar", null);
+                result.resolved(ImmutableSet.of(artifact));
+            }
+        }
+
+        @Override
+        protected void resolveJavadocArtifacts(ModuleComponentResolveMetaData module, BuildableArtifactSetResolveResult result) {
+            // Javadoc artifacts are optional, so we need to probe for them remotely
+        }
+
+        @Override
+        protected void resolveSourceArtifacts(ModuleComponentResolveMetaData module, BuildableArtifactSetResolveResult result) {
+            // Javadoc artifacts are optional, so we need to probe for them remotely
+        }
+    }
+
+    private class P2RemoteRepositoryAccess extends RemoteRepositoryAccess {
+        @Override
+        protected void resolveConfigurationArtifacts(ModuleComponentResolveMetaData module, ConfigurationMetaData configuration, BuildableArtifactSetResolveResult result) {
+            MavenModuleResolveMetaData mavenMetaData = mavenMetaData(module);
+            if (mavenMetaData.isPomPackaging()) {
+                Set<ComponentArtifactMetaData> artifacts = new LinkedHashSet<ComponentArtifactMetaData>();
+                artifacts.addAll(findOptionalArtifacts(module, "jar", null));
+                result.resolved(artifacts);
+            } else {
+                ModuleComponentArtifactMetaData artifactMetaData = module.artifact(mavenMetaData.getPackaging(), mavenMetaData.getPackaging(), null);
+
+                if (createArtifactResolver(module.getSource()).artifactExists(artifactMetaData, new DefaultResourceAwareResolveResult())) {
+                    result.resolved(ImmutableSet.of(artifactMetaData));
+                } else {
+                    ModuleComponentArtifactMetaData artifact = module.artifact("jar", "jar", null);
+                    result.resolved(ImmutableSet.of(artifact));
+                }
+            }
+        }
+
+        @Override
+        protected void resolveJavadocArtifacts(ModuleComponentResolveMetaData module, BuildableArtifactSetResolveResult result) {
+            result.resolved(findOptionalArtifacts(module, "javadoc", "javadoc"));
+        }
+
+        @Override
+        protected void resolveSourceArtifacts(ModuleComponentResolveMetaData module, BuildableArtifactSetResolveResult result) {
+            result.resolved(findOptionalArtifacts(module, "source", "sources"));
+        }
+    }
+}
---
+++ subprojects/dependency-management/src/test/groovy/org/gradle/api/internal/artifacts/repositories/DefaultP2ArtifactRepositoryTest.groovy
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.internal.artifacts.repositories
+
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.artifacts.repositories.PasswordCredentials
+import org.gradle.api.internal.artifacts.ivyservice.ivyresolve.strategy.ResolverStrategy
+import org.gradle.api.internal.artifacts.repositories.resolver.IvyResolver
+import org.gradle.api.internal.artifacts.repositories.resolver.P2Resolver
+import org.gradle.api.internal.artifacts.repositories.transport.RepositoryTransport
+import org.gradle.api.internal.artifacts.repositories.transport.RepositoryTransportFactory
+import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.internal.filestore.ivy.ArtifactIdentifierFileStore
+import org.gradle.internal.reflect.DirectInstantiator
+import org.gradle.internal.resource.local.LocallyAvailableResourceFinder
+import org.gradle.internal.resource.transport.ExternalResourceRepository
+import org.gradle.logging.ProgressLoggerFactory
+import spock.lang.Specification
+
+class DefaultP2ArtifactRepositoryTest extends Specification {
+    final FileResolver fileResolver = Mock()
+    final PasswordCredentials credentials = Mock()
+    final RepositoryTransportFactory transportFactory = Mock()
+    final LocallyAvailableResourceFinder locallyAvailableResourceFinder = Mock()
+    final ExternalResourceRepository resourceRepository = Mock()
+    final ProgressLoggerFactory progressLoggerFactory = Mock()
+    final ResolverStrategy resolverStrategy = Stub()
+    final ArtifactIdentifierFileStore artifactIdentifierFileStore = Stub()
+
+    final DefaultP2ArtifactRepository repository = new DefaultP2ArtifactRepository(
+            fileResolver, credentials, transportFactory, locallyAvailableResourceFinder,
+            new DirectInstantiator(), resolverStrategy, artifactIdentifierFileStore
+    )
+
+    def "default values"() {
+        expect:
+        repository.url == null
+    }
+
+    def "creates a resolver for HTTP url"() {
+        repository.name = 'name'
+        repository.url 'http://host/'
+
+        given:
+        fileResolver.resolveUri('http://host/') >> new URI('http://host/')
+        transportFactory.createTransport({ it == ['http'] as Set }, 'name', credentials) >> transport()
+
+
+        when:
+        def resolver = repository.createResolver()
+
+        then:
+        with(resolver) {
+            it instanceof P2Resolver
+            repository == resourceRepository
+            name == 'name'
+            url == new URI('http://host/')
+        }
+    }
+
+    def "creates a resolver for file patterns"() {
+        repository.name = 'name'
+        def file = new File("test")
+        def fileUri = file.toURI()
+
+        given:
+        fileResolver.resolveUri('repo/') >> fileUri
+        transportFactory.createTransport({ it == ['file'] as Set }, 'name', credentials) >> transport()
+
+        when:
+        def resolver = repository.createResolver()
+
+        then:
+        with(resolver) {
+            it instanceof P2Resolver
+            repository instanceof ExternalResourceRepository
+            name == 'name'
+            url == fileUri
+        }
+    }
+
+    private RepositoryTransport transport() {
+        return Mock(RepositoryTransport) {
+            getRepository() >> resourceRepository
+        }
+    }
+}
